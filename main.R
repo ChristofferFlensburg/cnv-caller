c320f2f912eaab720001dec687a8fc8c73f6211d
Christoffer Flensburg <flensburg.c@mac2561.local>
2014-11-18 10:34:52 +1100

    Added main.R, the main file the user will set up and run the analysis from.

---
+++ main.R
@@ -0,0 +1,34 @@
+
+###############################################################################
+#
+# The main analysis function, calling this will analyse everything and produce
+# most of the plots and output you need for a paper. The current version will not
+# write the paper for you.
+#
+# Input:
+#   inputFiles:
+#     A named list with entries
+#     'metaData' absolute path to the tab separated meta data file.
+#     'vcfFiles' vector of absolute paths to vcf files.
+#
+#  outputDirectories:
+#    A named list with entries
+#    'R' absolute path to the directory where data from the analysis will be stored.
+#    'plots' absolute path to the directory where plots will be placed.
+#
+#  forceRedo:
+#    A named logical vector indicating what steps should be forced to be redone,
+#    even if saved results are already in place. forceRedo() gives the default
+#    setting with all the possible steps set to FALSE.
+#
+###############################################################################
+analyse = function(inputFiles, outputDirectories, forceRedo, runtimeSettings) {
+  
+
+  
+}
+
+
+forceRedo = function() {
+
+}
